9b854568222d607bcbf4c8900c8972b465da9e52
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-06-23 10:37:36 +0530

    All Done Again

--- core-java-practice/gcr-codebase/exception-practices/Class-work-Example/ArithmeticExceptionExample.java
+++
@@ -1,9 +0,0 @@
-public class ArithmeticExceptionExample {
-    public static void main(String[] args) {
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("ArithmeticException caught: " + e.getMessage());
-        }
-    }
-}
